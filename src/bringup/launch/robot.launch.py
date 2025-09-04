a418d9b1f50664c3fb3448cbc068449d75e6fab3
Keller <felipe.keller@icloud.com>
2025-09-04 10:59:02 -0300

    bringup

--- src/bringup/launch/robot.launch.py
+++ src/bringup/launch/robot.launch.py
@@ -12,9 +12,9 @@ def generate_launch_description():
     team_color_arg = DeclareLaunchArgument('team_color', default_value='blue')
     player_number_arg = DeclareLaunchArgument('player_number', default_value='1')
     role_arg = DeclareLaunchArgument('role', default_value='striker')
-    camera_type_arg = DeclareLaunchArgument('camera_type', default_value='csi')
-    debug_arg = DeclareLaunchArgument('debug', default_value='false')
-    model_path_arg = DeclareLaunchArgument('model_path', default_value='resources/models/robocup_yolov8.pt')
+    camera_type_arg = DeclareLaunchArgument('camera_type', default_value='usb')
+    debug_arg = DeclareLaunchArgument('debug', default_value='true')
+    model_path_arg = DeclareLaunchArgument('model_path', default_value='/ros2_ws/src/perception/resources/models/robocup_yolov8.engine')
 
     team_color = LaunchConfiguration('team_color')
     player_number = LaunchConfiguration('player_number')
